d9fa6e59f3358f247121f45f15c7c5036dd5b861
minnug <parkminnug@gmail.com>
2024-04-04 20:46:09 +0900

    ✨ 영화 리스트 - Row 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
 import Category from './components/Category';
+import Row from './components/Row';
+import requests from './api/request';
 
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
       <Nav />
       <Banner />
       <Category />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Action Moives" id="AM" fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}></Row>
     </Container>
   );
 }
